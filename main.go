b83481f1e73866b0eb521ec0b995952a9ac98ec6
Norbert Fuhs <norbert.fuhs.xyz@gmail.com>
2017-08-16 15:57:46 +0200

    Added base files

--- main.go
+++ main.go
@@ -1,11 +1,11 @@
 package main
 
-import "net/http"
+import (
+	"log"
+	"net/http"
+)
 
 func main() {
-	http.Handle("/", http.FileServer(http.Dir("public/index.html")))
-	err := http.ListenAndServe(":8000", nil)
-	if err != nil {
-		panic(err)
-	}
+	// Simple static webserver:
+	log.Fatal(http.ListenAndServe(":8080", http.FileServer(http.Dir("public"))))
 }
